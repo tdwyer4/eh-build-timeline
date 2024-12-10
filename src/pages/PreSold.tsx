63d7c6458c10aaba598ff38e1fe7b6308cb2b332
gutterpixel <jack@executivehomes.com>
2024-12-10 13:33:09 -0600

    nav

--- src/pages/PreSold.tsx
+++ src/pages/PreSold.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-import Hero from "../components/Hero/Hero";
+import React, { useEffect, useMemo, useState } from "react";
 import ProgressBar from "../components/ProgressBar/ProgressBar";
 import { GetStarted } from "../components/GetStarted/GetStarted";
 import PAList from "../components/PAList/PAList";
@@ -15,49 +14,86 @@ import { MasonryVideo } from "../components/VidSection/Masonry";
 import Closing from "../components/Closing/Closing";
 import { PunchoutVideo } from "../components/VidSection/PunchoutVideo";
 import Warranty from "../components/Warranty/Warranty";
-import styles from "./FullPageStyles.module.css";
 import CTA from "../components/Cta/Cta";
+import styles from "./FullPageStyles.module.css";
 
 interface PreSoldProps {
   pageTitle: string;
 }
 
+const sectionsData = [
+  {
+    categoryName: "Get Started",
+    items: [
+      { id: "2", title: "Get Started", component: <GetStarted /> },
+      { id: "3", title: "Purchase Agreement", component: <PAList /> },
+    ],
+  },
+  {
+    categoryName: "Foundation",
+    items: [{ id: "4", title: "Foundation", component: <FootingVideo /> }],
+  },
+  {
+    categoryName: "Communication",
+    items: [{ id: "5", title: "Communication", component: <Communication /> }],
+  },
+  {
+    categoryName: "Rough Trades",
+    items: [
+      { id: "6", title: "Framing", component: <FramingVideo /> },
+      { id: "8", title: "Rough Trades", component: <RoughVideo /> },
+    ],
+  },
+  {
+    categoryName: "Quality",
+    items: [
+      { id: "7", title: "Selections", component: <Selections /> },
+      { id: "9", title: "Quality", component: <ConstructionQuality /> },
+      { id: "10", title: "Insulation", component: <InsulationVideo /> },
+    ],
+  },
+  {
+    categoryName: "Interior",
+    items: [{ id: "11", title: "Finish", component: <FinishQuality /> }],
+  },
+  {
+    categoryName: "Exterior",
+    items: [{ id: "12", title: "Exterior", component: <MasonryVideo /> }],
+  },
+  {
+    categoryName: "Close",
+    items: [
+      { id: "13", title: "Close", component: <Closing /> },
+      { id: "14", title: "Punch", component: <PunchoutVideo /> },
+      { id: "15", title: "Warranty", component: <Warranty /> },
+      { id: "16", title: "Let's build!", component: <CTA /> },
+    ],
+  },
+];
+
 const PreSold: React.FC<PreSoldProps> = ({ pageTitle }) => {
   const [activeIndex, setActiveIndex] = useState(0);
-  const [progress, setProgress] = useState(0);
-  const [navExpanded, setNavExpanded] = useState(false);
 
-  const sections = [
-    { id: "2", title: "Get Started", component: <GetStarted /> },
-    { id: "3", title: "Purchase Agreement", component: <PAList /> },
-    { id: "4", title: "Foundation", component: <FootingVideo /> },
-    { id: "5", title: "Communication", component: <Communication /> },
-    { id: "6", title: "Framing", component: <FramingVideo /> },
-    { id: "7", title: "Selections", component: <Selections /> },
-    { id: "8", title: "Rough Trades", component: <RoughVideo /> },
-    { id: "9", title: "Quality", component: <ConstructionQuality /> },
-    { id: "10", title: "Insulation", component: <InsulationVideo /> },
-    { id: "11", title: "Finish", component: <FinishQuality /> },
-    { id: "12", title: "Exterior", component: <MasonryVideo /> },
-    { id: "13", title: "Close", component: <Closing /> },
-    { id: "14", title: "Punch", component: <PunchoutVideo /> },
-    { id: "15", title: "Warranty", component: <Warranty /> },
-    { id: "16", title: "Let's build!", component: <CTA /> },
-  ];
+  const progressBarItems = useMemo(
+    () =>
+      sectionsData.map((category) => ({
+        id: category.categoryName,
+        title: category.categoryName,
+        subItems: category.items.map((item) => ({
+          id: item.id,
+          title: item.title,
+        })),
+      })),
+    []
+  );
 
   useEffect(() => {
     const observer = new IntersectionObserver(
       (entries) => {
-        let mostVisibleEntry: IntersectionObserverEntry | null =
-          null as IntersectionObserverEntry | null;
+        let mostVisibleEntry: IntersectionObserverEntry | null = null as IntersectionObserverEntry | null;
 
         entries.forEach((entry) => {
-          const sectionId = parseInt(
-            entry.target.getAttribute("id") || "0",
-            10
-          );
-
-          if (entry.isIntersecting && !isNaN(sectionId)) {
+          if (entry.isIntersecting) {
             if (
               !mostVisibleEntry ||
               entry.intersectionRatio > mostVisibleEntry.intersectionRatio
@@ -67,43 +103,41 @@ const PreSold: React.FC<PreSoldProps> = ({ pageTitle }) => {
           }
         });
 
-        if (mostVisibleEntry) {
-          const visibleSectionId = parseInt(
-            mostVisibleEntry.target.getAttribute("id") || "0",
-            10
-          );
-          setActiveIndex(visibleSectionId - 2);
-          setProgress(((visibleSectionId - 2) / (sections.length - 1)) * 100);
+        if (mostVisibleEntry && mostVisibleEntry.target instanceof HTMLElement) {
+          const visibleSectionId = mostVisibleEntry.target.getAttribute("id");
+          if (visibleSectionId) {
+            const index = sectionsData.findIndex((section) =>
+              section.items.some((item) => item.id === visibleSectionId)
+            );
+            setActiveIndex(index);
+          }
         }
       },
       { threshold: 0.1 }
     );
 
-    const sectionElements: NodeListOf<HTMLElement> =
-      document.querySelectorAll("section[id]");
+    const sectionElements = document.querySelectorAll("section[id]");
     sectionElements.forEach((section) => observer.observe(section));
 
     return () => observer.disconnect();
-  }, [sections]);
-
-  const toggleNav = () => setNavExpanded(!navExpanded);
+  }, []);
 
   return (
     <div className={styles.pageContent}>
-      <div className={styles.progress}>
-        <ProgressBar
-          activeIndex={activeIndex}
-          items={sections.map(({ id, title }) => ({ id, title }))}
-          pageTitle={pageTitle}
-          variant="dots"
-        />
-      </div>
+      <ProgressBar
+        activeIndex={activeIndex}
+        items={progressBarItems}
+        pageTitle={pageTitle}
+        variant="dots"
+      />
       <div className={styles.sections}>
-        {sections.map(({ id, component }) => (
-          <section id={id} key={id} data-section={id}>
-            {component}
-          </section>
-        ))}
+        {sectionsData.flatMap((category) =>
+          category.items.map(({ id, component }) => (
+            <section key={id} id={id}>
+              {component}
+            </section>
+          ))
+        )}
       </div>
     </div>
   );
