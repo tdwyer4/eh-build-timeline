ff9d660e79011299aff7164d43cabc69a59fdcfd
Kevin Luu <this.kevinluu@gmail.com>
2024-12-12 13:57:39 -0600

    Updates

--- src/pages/FirstLoad.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function FirstLoad() {
-  return <div></div>;
-}
-
-export default FirstLoad;
--- src/pages/SplineHero.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./SplineHero.module.css";
-import Spline from "@splinetool/react-spline";
-
-export default function SplineHero() {
-  return (
-    <Spline scene="https://prod.spline.design/ljBCZPfzU8ZWXhR2/scene.splinecode" />
-  );
-}
